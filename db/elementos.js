4d6adcc0f4475b7bc601522d58f14f089f7281e8
giulianacarabettaow <gcarabettaowens@udesa.edu.ar>
2023-04-06 11:17:22 -0300

    avance pero rompi todo. Hay que revisar que esta rompiendo, pero pude conectar algunas cosas

--- db/elementos.js
+++ db/elementos.js
@@ -4,44 +4,47 @@ let listaProductos ={
         {
         id: 1,
         nombre: "Vela Roma",
-        categoría: "Velas aromáticas",
+        categ: "Velas aromáticas",
         precio: "$5.000",
-        descripción: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  "
+        descrip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  ",
+        img: function(req, res){
+            require('../public/images/products/alfombra-blanca.jpg')
+        },
         },
         {
         id: 2,
         nombre: "Vela Turín",
-        categoría: "Velas aromáticas",
+        categ: "Velas aromáticas",
         precio: "$5.500",
-        descripción: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  "
+        descrip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  "
         },
         {
         id: 3,
         nombre: "Plumón Danés",
-        categoría: "Ropa de cama",
+        categ: "Ropa de cama",
         precio: "$100.000",
-        descripción: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  "
+        descrip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  "
         },
         {
         id: 4,
         nombre: "Juego de sábanas Danés",
-        categoría: "Ropa de cama",
+        categ: "Ropa de cama",
         precio: "$80.000",
-        descripción: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  "
+        descrip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  "
         },
         {
         id: 5,
         nombre: "Maceta Tailandia",
-        categoría: "Botánicos",
+        categ: "Botánicos",
         precio: "$10.000",
-        descripción: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  "
+        descrip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  "
         },  
         {
         id: 6,
         nombre: "Maceta colgante Malasia",
-        categoría: "Botánicos",
+        categ: "Botánicos",
         precio: "$8.000",
-        descripción: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  "
+        descrip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis elementum massa. Proin mauris augue, maximus a nibh in, consectetur iaculis neque. Morbi quis ultricies neque. Suspendisse sed pellentesque purus, ac pretium tortor.  "
         }, 
     ]
 }
