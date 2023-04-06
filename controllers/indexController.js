ceb277de910be9e4acd28000647a94be74fd0d8d
IgnacioTettamanti <itettamanti@udesa.edu.ar>
2023-04-06 16:28:56 -0300

    Avances en ruteo

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,7 @@
-let listaProductos= require('../db/elementos');
+let baseDeDatosLocal= require('../db/data');
 let indexController ={
     inicio: function(req,res) {
-       return res.render('index', {listaProductos});
+       return res.render('index', {baseDeDatosLocal});
       }
 }
 
--- db/data.js
+++ db/data.js
@@ -1,76 +1,88 @@
 let baseDeDatosLocal = {
-    usuario = {
+    usuario: {
         usuario:'',
         email: 'juanperez@example.com',
-        fecha de nacimiento:'' ,
-        nro de documento:'' , 
+        fechaDeNacimiento:'' ,
+        nroDeDocumento:'' , 
         contraseña:'' ,
-        foto de perfil:'',
+        fotoDePerfil:'',
 
-    }; 
+    },
 
-    productos = [
+    productos: [
         {
             id:1,
             nombre: 'Alfombra Blanca',    
             descripcion:'No importaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
             precio:'$200.000', 
             imagen: '../public/imagenes/alfombra-blanca.jpg',
-        }
+        },
         {
             id:2,
             nombre:'Alfombra Trenzada',
             descripcion:'No importaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
             precio:'', 
             imagen:'',
-        }{
+        },
+        {
             id:3,
             nombre:'AlfombraUno',
             descripcion:'No importaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
             precio:'', 
             imagen:'',
-        }{
+        },
+        {
             id:4,
             nombre:'Cubre Maceta',
             descripcion:'No importaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
             precio:'', 
             imagen:'',
-        }{
+        },
+        {
             id:5,
             nombre:'Cafetera Moulinex',
             descripcion:'No importaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
             precio:'', 
             imagen:'',
-        }{
+        },
+        {
             id:6,
             nombre:'MacBook Pro',
             descripcion:'No importaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
             precio:'', 
             imagen:'',
-        }{
+        },
+        {
             id:7,
             nombre:'Samsung Galaxy S10',
             descripcion:'No importaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
             precio:'', 
             imagen:'',
-        }{
+        },
+        {
             id:8,
             nombre:'Tv Samsung Smart',
             descripcion:'No importaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
             precio:'', 
             imagen:'',
-        }{
+        },
+        {
             id:9,
             nombre:'Sofa Blanco',
             descripcion:'No importaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
             precio:'', 
             imagen:'',
-        }{
+        },
+        {
             id:10,
             nombre:'Libertad',
             descripcion:'No importaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
             precio:'', 
             imagen:'',
         }
+
+       
     ]
 }
+
+module.exports = baseDeDatosLocal;
