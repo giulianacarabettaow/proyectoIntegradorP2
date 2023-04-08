cd72b6b9bccbb17b60f0c47dd25c6f325de2c4cf
IgnacioTettamanti <itettamanti@udesa.edu.ar>
2023-04-08 11:54:47 -0300

    .

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,10 +1,10 @@
-//let db = require('../db/data')
+
 let dbProduct= require('../db/data')
 
 let productsController = {
 
     index: function(req,res){
-        return res.send('index')
+        return res.send(dbProduct)
     },
 
     add: function(req,res){
@@ -14,9 +14,18 @@ let productsController = {
         return res.render('search-results')
     },
     showProducts: function(req,res){
-        return res.render('products', {productUnique: dbProduct.productos})
-    }
+        let id = req.params.id
+        let resultado = []
 
+        for(let i=0; i<dbProduct.productos.length;i++){
+            if(dbProduct.productos[i].id == id){
+                resultado.push(dbProduct.productos[i])
+            }
+        }
+        
+        return res.render('products'); // ya se que para renderizar tengo que cambiar el SEND por un RENDER y pasar el archivo en el cual se renderiza y los parametros que tiene que tener en cuenta
+    }
+    
 }
 
 module.exports = productsController;
--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 let express = require ('express');
 let router = express.Router();
+
 let productsController = require('../controllers/productsController');
 
 
