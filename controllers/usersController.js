0e20ba751e2d490abeca923c9c37f30039922ac7
IgnacioTettamanti <itettamanti@udesa.edu.ar>
2023-06-13 00:54:03 -0300

    .

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -33,46 +33,46 @@ let usersController={
         // return res.redirect('/users/profile/:id') //y ese ID de donde lo sacamos? cual es? create, update o demas. Comparar con la base de datos
      },
 
-//      processLogin: function(req,res){
-//          let errors = {}; //objeto vacío al que le agregamos métodos
-//          if (req.body.email ==''){
-//              errors.message = 'Debes ingresar tu email'; //message es un método de errors
-//              res.locals.errors= errors; //asi pasamos el error a la vista
+    //      processLogin: function(req,res){
+    //          let errors = {}; //objeto vacío al que le agregamos métodos
+        //          if (req.body.email ==''){
+    //              errors.message = 'Debes ingresar tu email'; //message es un método de errors
+    //              res.locals.errors= errors; //asi pasamos el error a la vista
             
-//             return res.render('login'); //vuelve al login
-//          } else if(req.body.contr == ''){
-//              errors.message = 'Debes ingresar tu contraseña';
-//              res.locals.errors= errors;
-//              return res.render('login');
-//          }else{
-//              db.Usuario.findOne({
-//                  where: [
-//                      {email: req.body.email}
-//                  ]})
-//             .then((resultado)=>{
-//                  if (resultado != null){ //condicional anidado
-//                      let check = bcrypt.compareSync(req.body.contr, contrHasheada) //si hay coincidencia con el mail, comparar la contraseña con el hash
-//                      if(check == true){
-//                          req.session.usuario = resultado.dataValues;
-//                          if(req.body.recuerdame != undefined){
-//                              res.cookie('usuarioId', resultado.dataValues.id, {maxAge : 1000* 60 * 5 })
-//                          }
-//                          return res.redirect('profile'); //si el usuario existe, redirigir al perfil
-//                      } else {
-//                              errors.message = 'La contraseña es incorrecta'; 
-//                              res.locals.errors=errors;
-//                              return res.render ('login');}
-//                  } else{
-//                      errors.message= 'Este usuario no se encuentra registrado'; //en las vistas podemos hacer un condicional para que, junto con este mensaje muestre un link a la página de registro
-//                      res.locals.errors=errors;
-//                      return res.render('login');
-//                  }
-//              })
-//          .catch(function(error){
-//              console.log(error);
-//          })
-//      }
-//  },
+    //             return res.render('login'); //vuelve al login
+    //          } else if(req.body.contr == ''){
+    //              errors.message = 'Debes ingresar tu contraseña';
+    //              res.locals.errors= errors;
+    //              return res.render('login');
+    //          }else{
+    //              db.Usuario.findOne({
+    //                  where: [
+    //                      {email: req.body.email}
+    //                  ]})
+    //             .then((resultado)=>{
+    //                  if (resultado != null){ //condicional anidado
+    //                      let check = bcrypt.compareSync(req.body.contr, contrHasheada) //si hay coincidencia con el mail, comparar la contraseña con el hash
+    //                      if(check == true){
+    //                          req.session.usuario = resultado.dataValues;
+    //                          if(req.body.recuerdame != undefined){
+    //                              res.cookie('usuarioId', resultado.dataValues.id, {maxAge : 1000* 60 * 5 })
+    //                          }
+    //                          return res.redirect('profile'); //si el usuario existe, redirigir al perfil
+    //                      } else {
+    //                              errors.message = 'La contraseña es incorrecta'; 
+    //                              res.locals.errors=errors;
+    //                              return res.render ('login');}
+    //                  } else{
+    //                      errors.message= 'Este usuario no se encuentra registrado'; //en las vistas podemos hacer un condicional para que, junto con este mensaje muestre un link a la página de registro
+    //                      res.locals.errors=errors;
+    //                      return res.render('login');
+    //                  }
+    //              })
+    //          .catch(function(error){
+    //              console.log(error);
+    //          })
+    //      }
+    //  },
 
   
     logout: function(req, res){
@@ -103,57 +103,10 @@ let usersController={
     register: function(req,res){
         return res.render('register')
     },
-    // processRegister: function(req,res){          
-    //     db.Usuario.findOne({ //pedido asincronico
-    //         where: [{email: req.body.email}]
-    //     })
-    //     .then((result)=>{ 
-    //         let errors= {};
-    //         if (result != undefined){ //si existe un mail registrado
-    //             errors.message= "Este email ya fue registrado"; 
-    //             res.locals.errors= errors.message;   //entonces quiero que veas un mensaje de que ya estas registrado
-    //             return res.render('register');
-    //         }else{  //MENSAJES DE ERROR PARA LOS USUARIOS
-    //                 
-    //                 
-    //               
-    //                     db.Usuario.create(
-    //                         {
-    //                             email: req.body.email,
-    //                             nombre: req.body.nombre,
-    //                             contr: contrHasheada,
-    //                             fotoDePerfil: '',
-    //                             fechaDeNacimiento: req.body.fechaDeNacimiento,
-    //                             dni: req.body.dni,
-    //                             createdAt: req.body.createdAt,
-    //                         })
-    //                     .then((result)=>{
-    //                         return res.redirect('profile')
-    //                     })
-    //                     .catch((error)=>{
-    //                         console.log(error)
-    //                     });
-
-    //                 }
-    //             } //cierra el else grande
-    //         }) 
-    //         .catch((error)=>{
-    //             console.log(error)
-    //         }) 
-    // },
 
     processRegister: function(req,res){ //no anda todavia bien pero trae la data 
+       
         let userData = req.body
-
-        let newUser = {
-            nombre: userData.username,
-            email: userData.email,
-            contr: userData.password,
-            fechaDeNacimiento:userData.fechaNacimiento,
-            fotoDePerfil:userData.fotoPerfil,
-            dni:userData.dni
-        }
-        users.create(newUser)
         users.findOne( {where: 
             [{email: userData.email}]//,
            // { dni: userData.dni}] 
@@ -168,16 +121,17 @@ let usersController={
                 if(req.body.dni == ''){ 
                     errors.message = "Debes ingresar tu D.N.I." //aviso al usuario
                     res.locals.errors = errors
-                    return res.render('register')} 
-                } else if(req.body.fechaDeNacimiento == ''){
+                    return res.render('register') 
+                } 
+                else if (req.body.fechaDeNacimiento == ''){
                     errors.message= "Debes ingresar tu fecha de nacimiento" 
                     res.locals.errors= errors   
                     return res.render('register')
                 } else if(req.body.nombre== ''){
                     errors.message= "Debes ingresar tu nombre"
-                    res.locals.errors= errors.message   
+                    res.locals.errors= errors   
                     return res.render('register')
-                }} else if(req.body.email== ''){
+                } else if(req.body.email== ''){
                     errors.message= "Debes ingresar un email" 
                     res.locals.errors= errors   
                     return res.render('register')
@@ -185,23 +139,40 @@ let usersController={
                     errors.message= "Debes ingresar una contraseña" 
                     res.locals.errors= errors   
                     return res.render('register')
-                } else if(req.body.contr.length < 3){
-                    errors.message= "La contraseña debe contener más de 3 caracteres" 
-                    res.locals.errors= errors   
-                    return res.render('register')
-                } else{
-                    let contrHasheada = bcrypt.hashSync(req.body.contr, 10)
-                    if (req.file != undefined){ //ver el req.file
-                    datosDelUsuario.fotoDePerfil = req.file.filename
-                    }} else {
-                    datosDelUsuario.fotoDePerfil = 'default_avatar.png'
+                } //else if(req.body.contr.length < 3){
+                   //  errors.message= "La contraseña debe contener más de 3 caracteres" 
+                   //  res.locals.errors= errors   
+                    // return res.render('register')
+                //} 
+                else{
+                    //let contrHasheada = bcrypt.hashSync(req.body.contr, 10)
+                    
+                    let form = req.body
+                    let newUser = {
+                        nombre: form.username,
+                        email: form.email,
+                        contr: form.password,
+                        fechaDeNacimiento:form.fechaNacimiento,
+                        fotoDePerfil:form.fotoPerfil,
+                        dni:form.dni
                     }
+                    // if (req.file != undefined){ //ver el req.file
+                    //     datosDelUsuario.fotoDePerfil = req.file.filename
+                    //     } else {
+                    //     datosDelUsuario.fotoDePerfil = 'default_avatar.png'
+                    //     }
+                    users.create(newUser)
 
-            console.log(createdUser)
-            
-            return res.send(createdUser)
-           // return res.redirect('/users/login')
+                    .then(function(created){
+                        return res.send(created)
+                        //return res.redirect('/user/login')
+                    })
+                    .catch(function(error){
+                        console.log('El error es: ' + error)
+                    });
 
+                    }    
+             }    
         })
 
         .catch(function(error){
@@ -209,7 +180,7 @@ let usersController={
         })
     }
 
-} //cierra el modulo del controlador
+}//cierra el modulo del controlador
 
 
 module.exports = usersController;
